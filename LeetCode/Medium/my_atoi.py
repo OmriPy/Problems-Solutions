707961b841b3b9df006b056459997c3b0e8ed003
Omri <110406612+OmriPy@users.noreply.github.com>
2023-12-05 22:26:10 +0200

    Add files via upload

---
+++ LeetCode/Medium/my_atoi.py
@@ -0,0 +1,38 @@
+def add_digit(n: int, c: str) -> int:
+    return n * 10 + int(c)
+
+def myAtoi(s: str) -> int:
+    if len(s) > 200:
+        return 0
+    n = 0
+    sign = 0
+    signs = '+-'
+    started = False     # determines wether the digits enumeration started or not
+
+    for c in s:
+        if (c == ' ' and started) or \
+            (c == ' ' and sign != 0):
+            break
+        elif c == ' ':
+            continue
+        if not started and c.isdigit():
+            started = True
+        if (not c.isdigit() and c not in signs) or \
+            (c in signs and sign != 0) or \
+            (started and c in signs):
+            break
+        if c == '+':
+            sign = 1
+        elif c == '-':
+            sign = -1
+        if c.isdigit() and c not in signs:
+            n = add_digit(n, c)
+
+    if sign == 0:
+        sign = 1
+    val = n * sign
+    if val < -2**31:
+        return -2**31
+    if val > 2**31 - 1:
+        return 2**31 - 1
+    return val
